bb7c0353d73216479d78edaf2d549e8269599738
Callie Porter-Borden <porterhyphenborden@gmail.com>
2019-06-14 19:37:30 -0400

    added security

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const MOVIEDEX = require('./moviedex.json');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
 
-app.use((req, res) => {
-    res.send('Hello, gurl!');
+app.use(function validateBearerToken(req, res, next) {
+    const authToken = req.get('Authorization');
+    const apiToken = process.env.API_TOKEN;
+    
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' });
+    }
+    next();
 })
+
+function handleGetMovies(req, res) {
+    let response = MOVIEDEX;
+
+    if (req.query.genre) {
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        );
+    }
+    if (req.query.country) {
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        );
+    }
+    if (req.query.avg_vote) {
+        let voteQuery = parseFloat(req.query.avg_vote, 10);
+        if (Number.isNaN(voteQuery)) {
+            return res.status(400).send('Average vote must be a number.')
+        }
+        else {
+            response = response.filter(movie =>
+                movie.avg_vote >= voteQuery);
+        }
+    }
+
+    res.json(response);
+}
+
+app.get('/movies', handleGetMovies);
 
 const PORT = 8000;
 app.listen(PORT, () => {
